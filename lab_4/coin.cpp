f9ec14d1c8bbf32705069d82bdf62d3a47b940fa
Michael Mafeni <mm17291@it075675.wks.bris.ac.uk>
2019-10-27 16:05:14 +0000

    changed convolution function to use Mat

--- lab_4/coin.cpp
+++ lab_4/coin.cpp
@@ -7,11 +7,11 @@
 using namespace cv;
 using namespace std;
 
-float convolution(Mat &img,int kernel[3][3],int y, int x){
+float convolution(Mat &img,Mat &kernel,int y, int x){
     int val = 0;
     for(int a = -1; a <= 1; a++){
         for(int b = -1; b <= 1; b++){
-            val += kernel[a + 1][b + 1] * img.at<uchar>(y - a,x - b);
+            val += kernel.at<int>(a + 1,b + 1) * img.at<uchar>(y - a,x - b);
         }
     }
     return val;
@@ -28,24 +28,23 @@ int main(int argc, char const *argv[]) {
     Mat gray_image;
 
     cvtColor(image,gray_image,CV_BGR2GRAY);
-    int grad_x[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
-    int grad_y[3][3] = {{1, 1, 1}, {1, 1, 1}, {1, 1, 1}};
+    // int grad_x[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+    Mat gradX = (Mat_<int>(3,3) << -1,0,1,-2,0,2,-1,0,1);
+    Mat gradY = gradX.t();
 
-
-    Mat temp(image.rows,image.cols, CV_32FC1,Scalar(0));
+    Mat tempX(image.rows,image.cols, CV_32FC1,Scalar(0));
     Mat resultX(image.rows,image.cols, CV_8UC1,Scalar(0));
 
 
 
     for(int y = 1; y < image.rows - 1; y++){
         for(int x = 1; x < image.cols - 1; x++){
-            float new_point = convolution(gray_image,grad_x,y,x);
-            temp.at<float>(y,x) = new_point;
-            // if (new_point < 0) std::cout << new_point << '\n';
+            float new_point = convolution(gray_image,gradX,y,x);
+            tempX.at<float>(y,x) = new_point;
 
         }
     }
-    normalize(temp,resultX,0,255,NORM_MINMAX);
+    normalize(tempX,resultX,0,255,NORM_MINMAX);
     imwrite("grad_x.jpg",resultX);
     return 0;
 }
