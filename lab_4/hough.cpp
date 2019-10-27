8f95b4910ab244b07d8f814385087f6a0d302942
M-Mafeni <mm17291@bristol.ac.uk>
2019-10-27 17:10:34 +0000

    changed hough.cpp

--- lab_4/hough.cpp
+++ lab_4/hough.cpp
@@ -3,9 +3,11 @@
 #include <opencv/cv.h>        //you may need to
 #include <opencv/highgui.h>   //adjust import locations
 #include <opencv/cxcore.h>    //depending on your machine setup
-#define THRESHOLD 127
+
 using namespace cv;
 using namespace std;
+
+#define THRESHOLD 127
 
 Mat threshold(Mat &image) {
     Mat thr(image.rows,image.cols,CV_8UC1,Scalar(0));
@@ -13,7 +15,8 @@ Mat threshold(Mat &image) {
         for(int x = 0; x < image.cols; x++){
             if(image.at<uchar>(y,x) > THRESHOLD){
                 thr.at<uchar>(y,x) = 255;
-            }else{
+            }
+            else{
                 thr.at<uchar>(y,x) = 0;
             }
         }
@@ -21,8 +24,14 @@ Mat threshold(Mat &image) {
     imwrite("thr.jpg",thr);
     return thr;
 }
+
+void hough(Mat &thr, Mat &dir) {
+
+}
+
 int main(int argc, char const *argv[]) {
-    Mat image = imread("mag.jpg",0);
-    threshold(image);
+    Mat mag = imread("mag.jpg",0);
+    Mat dir = imread("dir.jpg",0);
+    Mat thr = threshold(mag);
     return 0;
 }
