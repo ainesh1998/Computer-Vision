d76ab5931feac0bdc5d1e7aa3845c2a9dadb6abd
M-Mafeni <mm17291@bristol.ac.uk>
2019-11-07 17:53:27 +0000

    made dartboard classifier

--- coursework/face.cpp
+++ coursework/face.cpp
@@ -53,7 +53,7 @@ void detectAndDisplay( Mat frame )
 	cvtColor( frame, frame_gray, CV_BGR2GRAY );
 	equalizeHist( frame_gray, frame_gray );
 
-	// 2. Perform Viola-Jones Object Detection 
+	// 2. Perform Viola-Jones Object Detection
 	cascade.detectMultiScale( frame_gray, faces, 1.1, 1, 0|CV_HAAR_SCALE_IMAGE, Size(50, 50), Size(500,500) );
 
        // 3. Print number of Faces found
