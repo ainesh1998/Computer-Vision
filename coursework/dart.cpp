3528e111ceb78526e17fad5b262dc9fa8936225e
ainesh1998 <ainesh1998@outlook.com>
2019-11-22 13:01:08 +0000

    reorganised files and started task 2b

--- coursework/dart.cpp
+++ coursework/dart.cpp
@@ -15,13 +15,18 @@
 #include <stdio.h>
 #include "Hough.h"
 
+#define IOU_THRESHOLD 0.2
 
 using namespace std;
 using namespace cv;
 // using namespace hough;
 
 /** Function Headers */
-void detectAndDisplay( Mat frame );
+vector<Rect> detectAndDisplay( Mat frame );
+void drawTruth(Mat frame,int values[][4],int length);
+double true_pos_rate(vector<Rect> predictions,int truth_values[][4],int truth_length);
+double intersectionOverUnion(Rect prediction,int truth_value[4]);
+double calc_f1_score(vector<Rect> predictions,int truth_values[][4],int truth_length,double tpr);
 
 /** Global variables */
 String cascade_name = "dartcascade/cascade.xml";
@@ -31,15 +36,24 @@ CascadeClassifier cascade;
 /** @function main */
 int main( int argc, const char** argv )
 {
-	Hough hough;
-       // 1. Read Input Image
+	// Hough hough;
+    // 1. Read Input Image
 	Mat frame = imread(argv[1], CV_LOAD_IMAGE_COLOR);
-	hough.hough(frame);
+	// hough.hough(frame);
 	// 2. Load the Strong Classifier in a structure called `Cascade'
 	if( !cascade.load( cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
 
 	// 3. Detect Faces and Display Result
-	detectAndDisplay( frame );
+	vector<Rect> predictions;
+	predictions = detectAndDisplay( frame );
+
+	int ground_truth_vals[][4] = {{57,237,81,120},{823,198,156,163}};
+	int length = sizeof(ground_truth_vals)/sizeof(ground_truth_vals[0]);
+	drawTruth(frame,ground_truth_vals,length);
+	double tpr = true_pos_rate(predictions,ground_truth_vals,length);
+	printf("true pos rate = %f \n",tpr );
+	double f1_score = calc_f1_score(predictions,ground_truth_vals,length,tpr);
+	printf("f1 score = %f \n",f1_score);
 
 	// 4. Save Result Image
 	imwrite( "detected.jpg", frame );
@@ -49,7 +63,7 @@ int main( int argc, const char** argv )
 }
 
 /** @function detectAndDisplay */
-void detectAndDisplay( Mat frame )
+vector<Rect> detectAndDisplay( Mat frame )
 {
 	std::vector<Rect> faces;
 	Mat frame_gray;
@@ -59,7 +73,7 @@ void detectAndDisplay( Mat frame )
 	equalizeHist( frame_gray, frame_gray );
 
 	// 2. Perform Viola-Jones Object Detection
-	cascade.detectMultiScale( frame_gray, faces, 1.1, 10, 0|CV_HAAR_SCALE_IMAGE, Size(50, 50), Size(250,250) );
+	cascade.detectMultiScale( frame_gray, faces, 1.1, 5, 0|CV_HAAR_SCALE_IMAGE, Size(50, 50), Size(500,500) );
 
        // 3. Print number of Faces found
 	std::cout << faces.size() << std::endl;
@@ -69,5 +83,68 @@ void detectAndDisplay( Mat frame )
 	{
 		rectangle(frame, Point(faces[i].x, faces[i].y), Point(faces[i].x + faces[i].width, faces[i].y + faces[i].height), Scalar( 0, 255, 0 ), 2);
 	}
+	return faces;
+}
 
+///// FROM face.cpp /////
+
+void drawTruth(Mat frame,int values[][4],int length){
+	for(int i = 0; i < length; i++){
+		int x = values[i][0];
+		int y = values[i][1];
+		int width = values[i][2];
+		int height = values[i][3];
+		rectangle(frame,Point(x,y) ,Point(x+width,y+height),Scalar(0, 0, 255), 2);
+	}
+}
+//given predictions and truth values calculate the true positive rate (no of correct faces/no of valid faces)
+double true_pos_rate(vector<Rect> predictions,int truth_values[][4],int truth_length){
+	int detected;
+	double iou_scores[predictions.size() * truth_length];
+	for(int i = 0; i < predictions.size();i++){
+		for(int j = 0; j < truth_length; j++){
+			//compare each prediction with every truth value
+			//if they don't overlap IOU = 0
+			double iou = intersectionOverUnion(predictions[i],truth_values[j]);
+			iou_scores[truth_length * i + j] = iou;
+		}
+	}
+	//calculate no of detected faces
+	for(int i = 0; i < predictions.size() * truth_length; i++){
+		if(iou_scores[i] > IOU_THRESHOLD) detected++;
+	}
+	//detected = true positives
+	return (double)detected/(double)truth_length;
+}
+/*
+Given predictions and truth values, calculates the f1 score
+f1 score = 2 x ((precision * recall)/(precision + recall))
+precision = true positives/(true positives + false positives)
+recall (tpr) = true positives/(true positives + false negative)
+true positives + false negatives = truth_values
+therefore recall = tpr
+*/
+double calc_f1_score(vector<Rect> predictions,int truth_values[][4],int truth_length,double tpr){
+	double recall = tpr;
+	int true_positives = recall * truth_length;
+	double precision = (double)true_positives/(double)predictions.size();
+	printf("precision = %f\n",precision );
+	double f1_score = 2 * (precision * recall)/(precision + recall);
+	return f1_score;
+}
+//Compute the intersection over union between prediction and truth value boxes
+double intersectionOverUnion(Rect prediction,int truth_value[4]){
+	//determine coordinates of intersection rectangle
+	int x0 = max(prediction.x,truth_value[0]);
+	int y0 = max(prediction.y,truth_value[1]);
+	int x1 = min(prediction.x + prediction.width,truth_value[0] + truth_value[2]);
+	int y1 = min(prediction.y+ prediction.height,truth_value[1]+ truth_value[3]);
+	//find area of intersection rectangle
+	//0 is in case there's no intersect
+	double intersect_area = max(0,(x1 - x0)) * max(0,(y1 - y0));
+	double pred_area = prediction.width * prediction.height;
+	double truth_area = truth_value[2] * truth_value[3];
+	double iou = intersect_area/(pred_area + truth_area - intersect_area);
+	// printf("%f\n",iou );
+	return iou;
 }
