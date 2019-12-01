d4a5027f9e9d45b044e851d0901776c6a43b4993
ainesh1998 <ainesh1998@outlook.com>
2019-12-01 14:54:39 +0000

    redid graph

--- coursework/report/task_2a/graph_2a.py
+++ coursework/report/task_2a/graph_2a.py
@@ -3,9 +3,11 @@ import matplotlib.pyplot as plt
 
 fig, ax = plt.subplots()
 # ax.scatter(np.array([1, 0.023, 0.0015]), np.array([1,1,1]))
+ax.plot(np.array([0,1]), np.array([0,1]), linestyle="--", c="red", zorder=-1)
 ax.scatter(1,1, label='Stage 0')
 ax.scatter(0.023, 1, label='Stage 1')
 ax.scatter(0.0015, 1, label='Stage 2')
+plt.text(0.47,0.5, "TPR = FPR", fontsize=16, rotation=38, rotation_mode='anchor')
 plt.legend()
 plt.xlabel("False Positive Rate ")
 plt.ylabel("True Positive Rate")
